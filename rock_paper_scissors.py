c7a58cea9c671754d23ff9a0a210fcac11322a30
Westin Wilkins <108081477+westin-wilkins@users.noreply.github.com>
2023-02-22 16:49:23 -0600

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,8 @@
 import random
 
 # Rock, Paper, Scissors against the ai.
-# The program keeps track of number of wins between the player and computer.
-# Gives the score inbetween rounds.
+# Input either "rock", "paper", "scissors" to play or "q" to quit.
+# The program keeps track of number of computer and player wins.
 # Inputting q when prompted gives the final score.
 
 player_points = 0
@@ -14,40 +14,46 @@ while True:
     player_hand = input('Select either rock, paper, scissors, or q to quit: ')
     computer_hand = random.choice(possible_hands)
     print(f'Player chose {player_hand} and the computer chose {computer_hand}')
-
+    
     # Case were player selects rock
     if player_hand == 'rock':
         if computer_hand == 'scissors':
             print('Rock beats scissors! You win!')
             player_points += 1 
-            print(f"Player score: {player_points}")
+            print(f"Player score: {player_points}    Computer's score: {computer_points}")
+            print(" ")
         else:
             print('Paper beats rock! You lose!')
             computer_points += 1
-            print(f"Computer's score: {computer_points}")
+            print(f"Computer's score: {computer_points}    Player score: {player_points}")
+            print(" ")
     
     # Case were player selects paper
     if player_hand == 'paper':
         if computer_hand == 'rock':
             print('Paper beats rock! You win!')
             player_points += 1 
-            print(f"Player score: {player_points}")
+            print(f"Player score: {player_points}    Computer's score: {computer_points}")
+            print(" ")
         else:
             print('Scissors beat paper! You lose!')
             computer_points += 1
-            print(f"Computer's score: {computer_points}")
+            print(f"Computer's score: {computer_points}    Player score: {player_points}")
+            print(" ")
             
     # Case were player selects scissors
     if player_hand == 'scissors':
         if computer_hand == 'paper':
             print('Scissors beat paper! You win!')
             player_points += 1
-            print(f"Player score: {player_points}")
+            print(f"Player score: {player_points}    Computer's score: {computer_points}")
+            print(" ")
         else:
             print('Rock beats You lose!')
             computer_points += 1
-            print(f"Computer's score: {computer_points}")
-            
+            print(f"Computer's score: {computer_points}    Player score: {player_points}")
+            print(" ")
+    
     # Case were player selects q
     if player_hand == 'q':
         print(f'The player won {player_points} games and the computer won {computer_points} games')
@@ -57,4 +63,4 @@ while True:
             print("It's a tie!")
         else:
             print('The computer won. Try again next time!')
-        break        
+        break      
