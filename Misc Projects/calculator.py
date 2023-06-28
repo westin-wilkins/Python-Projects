e5926dde64da01a08655df1753164a0cbf1adfc3
Westin Wilkins <108081477+westin-wilkins@users.noreply.github.com>
2023-06-28 17:05:49 -0500

    calculator.py

--- Misc Projects/calculator.py
+++ Misc Projects/calculator.py
@@ -1,63 +1,77 @@
-import sys 
 import os
-
 
 def main():
     while True:
         try:
-            integers = input("Input two integers or type Quit: ").split()
+            integers = integer_checker()
             
-            if integers[0] == "quit":
-                    return False
-                
-            elif len(integers) > 2:
-                os.system('cls')
-                print("Too many integers!\n")
-            
-            elif integers.isdigit() == False:
-                os.system('cls')
-                print("Please only input integers.")
+            if integers == False:
+                return False
             
             else:
                 integer_1 = integers[0]
                 integer_2 = integers[1]
                 
-                command = input("| Addition | Subtraction | Multiplication | Division | ").lower()
-                
-                if command == "addition":
-                    add = addition(integer_1, integer_2)
-                    os.system('cls')
-                    print(f"{integer_1} + {integer_2} = {add}\n")
-                    
-                elif command == "subtraction":
-                    subtract = subtraction(integer_1, integer_2)
-                    os.system('cls')
-                    print(f"{integer_1} - {integer_2} = {subtract}\n")
-                    
-                elif command == "multiplication":
-                    multiply = multiplication(integer_1, integer_2)
-                    os.system('cls')
-                    print(f"{integer_1} x {integer_2} = {multiply}\n")
-                
-                elif command == "division":
-                    divide = division(integer_1, integer_2)
-                    os.system('cls')
-                    print(f"{integer_1} / {integer_2} = {divide}\n")
-                    
-                else:
-                    os.system('cls')
-                    print("Invalid input")
-            
+                math_operation(integer_1, integer_2)
+        # Handles the case when the user tries to divide by zero. Prevents the program
+        # from crashing
         except ZeroDivisionError:
                 os.system('cls')
                 print("Can't divide by zero\n")
-        except ValueError:
-            os.system('cls')
-            print("Invalid input\n")
-            
-            
-        
 
+# Function that checks the user's input to make sure that it is an integer or quit       
+def integer_checker():
+    while True:
+        user_input = input("Input two integers or type Quit: >")
+        
+        if user_input.lower() == 'quit':
+            os.system('cls')
+            return False
+
+        else:
+            integer_check = user_input.split()
+            # Checks if the user typed in to many numbers
+            if len(integer_check) > 2:
+                os.system('cls')
+                print("Too many integers!\n")
+            # Checks if the user typed in characters instead of numbers
+            elif not integer_check[0].isdigit() or not integer_check[1].isdigit():
+                os.system('cls')
+                print("Please only input integers")
+            # If the input is acceptable, the values are passed back to the main function
+            else:
+                return integer_check
+            
+# Function that performs the calculations depending on what is typed in
+def math_operation(integer_1, integer_2):
+    while True:
+        user_input = input("| Addition | Subtraction | Multiplication | Division | >").lower()
+        
+        if user_input == "addition":
+            add = addition(integer_1, integer_2)
+            os.system('cls')
+            print(f"{integer_1} + {integer_2} = {add}\n")
+            
+        elif user_input == "subtraction":
+            subtract = subtraction(integer_1, integer_2)
+            os.system('cls')
+            print(f"{integer_1} - {integer_2} = {subtract}\n")
+            
+        elif user_input == "multiplication":
+            multiply = multiplication(integer_1, integer_2)
+            os.system('cls')
+            print(f"{integer_1} x {integer_2} = {multiply}\n")
+        
+        elif user_input == "division":
+            divide = division(integer_1, integer_2)
+            os.system('cls')
+            print(f"{integer_1} / {integer_2} = {divide}\n")
+            
+        else:
+            os.system('cls')
+            print("Invalid input")
+
+# All of the possible operations
 def addition(integer_1, integer_2):
     return int(integer_1) + int(integer_2)
 
