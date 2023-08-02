ecc64bb282e689c9643dfa810eb1b9d72a84f621
Westin Wilkins <108081477+westin-wilkins@users.noreply.github.com>
2023-08-02 02:22:00 -0500

    Delete pythagorean_triples.py

--- Math Projects/pythagorean_triples.py
+++
@@ -1,9 +0,0 @@
-
-def pythagorean_triples(n):
-    for z in range(3, n+1):
-        for y in range(3, z):
-            for x in range(3, y):
-                if x**2 + y**2 == z**2:
-                    print(x, y, z)
-
-pythagorean_triples(200)
