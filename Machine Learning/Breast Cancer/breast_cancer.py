199a87c7a4c1c20a2fe67b39fb0619bac1a6a5ac
Westin Wilkins <108081477+westin-wilkins@users.noreply.github.com>
2023-09-02 21:38:59 -0500

    Add files via upload

---
+++ Machine Learning/Breast Cancer/breast_cancer.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv("data.csv")
+
+X_train, X_test, y_train, y_test = train_test_split(df['radius_mean'], df['diagnosis'], random_state=0)
+
+X_train = np.array(X_train).reshape(-1, 1)
+X_test = np.array(X_test).reshape(-1, 1)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+# Values for prediction
+X_new = np.array([[25.22], [2.9], [1], [30]])
+
+prediction = knn.predict(X_new)
+
+# Plotting the radius values
+plt.scatter(X_train, y_train, c=y_train, cmap='viridis')
+plt.scatter(X_new, prediction)
+
+plt.xlabel('Radius Mean')
+plt.ylabel('Diagnosis')
+plt.title('Radius Mean vs Diagnosis')
+
+# 0 mean Benign | 1 means Malignant
+print("Prediction: {}".format(prediction))
+
+# Prints accuracy 
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+plt.show()
+
